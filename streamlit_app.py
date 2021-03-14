adb779bdb44d298e54b69c08d88b441b549945a2
Hamdymi <60826362+Hamdymi@users.noreply.github.com>
2021-03-14 14:22:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def load_data():
 html_temp = """
     <div class='tableauPlaceholder' id='viz1615584326049' style='position: relative'><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Bo&#47;Book1_16155695091580&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Book1_16155695091580&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Bo&#47;Book1_16155695091580&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1615584326049');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='900px';vizElement.style.height='527px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='900px';vizElement.style.height='527px';} else { vizElement.style.width='100%';vizElement.style.height='727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
     """
-components.html(html_temp, width=1000, height=1000)
+
 
 #The map ends here, we will move it as appropraite
 
@@ -87,7 +87,7 @@ second_lockdown_start = alt.Chart(df_cases_by_date).mark_rule().encode(
 text_first_lockdown = first_lockdown_start.mark_text(
     align='left',
     baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+    dx=3
 ).encode(
     text='lockdown_date:T'
 )
@@ -96,7 +96,7 @@ text_first_lockdown = first_lockdown_start.mark_text(
 text_second_lockdown = second_lockdown_start.mark_text(
     align='left',
     baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+    dx=3  
 ).encode(
     text='lockdown2_date:T'
 )
@@ -110,7 +110,7 @@ first_lockdown_end = alt.Chart(df_cases_by_date).mark_rule().encode(
 text_first_lockdown_end = first_lockdown_end.mark_text(
     align='left',
     baseline='middle',
-    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+    dx=3 
 ).encode(
     text='lockdown1_end:T'
 )
@@ -125,7 +125,7 @@ second_lockdown_end = alt.Chart(df_cases_by_date).mark_rule().encode(
 text_second_lockdown_end = second_lockdown_end.mark_text(
     align='left',
     baseline='middle',
-    dx=3,  # Nudges text to right so it doesn't appear on top of the bar
+    dx=3,  
     dy=20
 ).encode(
     text='lockdown2_end:T'
@@ -141,8 +141,13 @@ text_second_lockdown_end = second_lockdown_end.mark_text(
 
 combined = new_and_cum_cases+first_lockdown_start+second_lockdown_start +text_first_lockdown + text_second_lockdown + first_lockdown_end + text_first_lockdown_end + second_lockdown_end + text_second_lockdown_end
 to_plot1 = combined.interactive().properties(width=800, title='COVID-19 in Nigeria as at 3rd of March 2020 -- Green and black lines represent start and end of major restrictions resp.')
-st.write(to_plot1 | make_selector)
-
-
-
 #Situation in Nigeria at a Glance Ends
+
+
+
+
+
+
+#We put elements on screen here
+st.write(to_plot1 | make_selector)
+components.html(html_temp, width=1000, height=1000)
