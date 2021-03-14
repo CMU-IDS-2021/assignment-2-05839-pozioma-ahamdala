fc61b588d0eec4358558437f14c09a7e0905d2da
Hamdymi <60826362+Hamdymi@users.noreply.github.com>
2021-03-14 13:08:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,22 +26,30 @@ df = pd.read_excel('Copy of nga_subnational_covid19_hera.xlsx')
 
 
 
-picked=alt.selection_single(empty='none')
+#Situation in Nigeria at a Glance
 
-#The new cases and cumulative cases
 df['New Cases'] = df['CONTAMINES']
 df_cases_by_date = df.groupby(df['DATE']).sum()
 df_cases_by_date = df_cases_by_date.reset_index()
-df_cases_by_date['Active Cases (in hundreds)'] = (df_cases_by_date['CONTAMINES'].cumsum())/100
-df_cases_by_date['First lockdown'] = datetime.datetime(2020, 3, 30)
+df_cases_by_date['Active Cases (in hundreds)'] = ((df_cases_by_date['CONTAMINES'].cumsum()) - (df_cases_by_date['GUERIS'].cumsum()) + (df_cases_by_date['DECES'].cumsum()))/100
+#df_cases_by_date['Cumulative Change in Number of Cases'] = (df_cases_by_date['CONTAMINES'].cumsum())/100
 
-new_and_cum_cases = alt.Chart(df_cases_by_date).transform_fold(
-    ['New Cases', 'Active Cases (in hundreds)'],
-).mark_line().encode(
-    alt.X('DATE:T'),
-    alt.Y('value:Q', title=''),
-    color='key:N'
-)
+df_cases_by_date['Percentage change in active cases (from yesterday)'] = (df_cases_by_date['Active Cases (in hundreds)'].pct_change(fill_method='ffill'))*100
+
+
+make = pd.DataFrame({'Trendline': ['New Cases', 'Active Cases (in hundreds)', 'Percentage change in active cases (from yesterday)']})
+df2 = df_cases_by_date[['DATE', 'New Cases', 'Active Cases (in hundreds)', 'Percentage change in active cases (from yesterday)']] 
+df3 = df2.melt(id_vars=['DATE'], var_name='Trendline', value_name='value')
+selection = alt.selection_multi(fields=['Trendline'])
+color = alt.condition(selection, alt.Color('Trendline:N'), alt.value('lightgray'))
+make_selector = alt.Chart(make).mark_rect().encode(y='Trendline', color=color).add_selection(selection)
+new_and_cum_cases = alt.Chart(df3).mark_line().encode(x='DATE:T', y=alt.Y('value:Q', title= " "), color=alt.Color('Trendline:N'),
+                                                      tooltip=['DATE', 'value:Q']
+                                                      ).transform_filter(selection).interactive()
+
+
+
+
 
 
 lockdown_date = datetime.datetime(2020,3,30)
@@ -49,16 +57,86 @@ lockdown2_date = datetime.datetime(2020,12,21)
 no_of_cases_first_lockdown = df_cases_by_date.loc[ df_cases_by_date['DATE']==lockdown_date, 'CONTAMINES'].values[0]
 no_of_cases_second_lockdown = df_cases_by_date.loc[ df_cases_by_date['DATE']==lockdown2_date, 'CONTAMINES'].values[0]
 
+no_of_active_cases_first_lockdown = df_cases_by_date.loc[ df_cases_by_date['DATE']==lockdown_date, 'Active Cases (in hundreds)'].values[0]
+no_of_active_cases_second_lockdown = df_cases_by_date.loc[ df_cases_by_date['DATE']==lockdown2_date, 'Active Cases (in hundreds)'].values[0]
 
-lockdown_starts = {"DATE": [lockdown_date, lockdown2_date], "CONTAMINES": [no_of_cases_first_lockdown, no_of_cases_second_lockdown ]}
-first_lockdown_start = pd.DataFrame(lockdown_starts, columns=["DATE", "CONTAMINES"])
-lockdown_start = pd.DataFrame([{"DATE": lockdown_date, "CONTAMINES": no_of_cases_first_lockdown} ])
 
-first_lockdown_start_line = alt.Chart(first_lockdown_start).mark_point(size=300).encode(
-    alt.X('DATE:T', title=''), color=alt.value('red'), y='CONTAMINES:Q'
+lockdown_starts = {"DATE": [lockdown_date, lockdown2_date], "New Cases": [no_of_cases_first_lockdown, no_of_cases_second_lockdown ], "Active Cases (in hundreds)": [no_of_active_cases_first_lockdown, no_of_active_cases_second_lockdown ], "Label":["First Lockdown Starts", "Second Lockdown Starts"] }
+first_lockdown_start = pd.DataFrame(lockdown_starts, columns=["DATE", "New Cases", "Active Cases (in hundreds)", "Label"])
+lockdown_start = pd.DataFrame([{"DATE": lockdown_date, "New Cases": no_of_cases_first_lockdown} ])
+
+first_lockdown_start_line = alt.Chart(first_lockdown_start).mark_point(size=200).encode(
+    alt.X('DATE:T', title=''), color=alt.Color('Label:N'), y='New Cases:Q',
+        
+    tooltip=['DATE', 'New Cases', 'Active Cases (in hundreds)'],
+).interactive()
+
+df_cases_by_date['lockdown_date'] = lockdown_date 
+first_lockdown_start = alt.Chart(df_cases_by_date).mark_rule().encode(
+    x='lockdown_date', color=alt.value('green'))
+
+
+text_first_lockdown = first_lockdown_start.mark_text(
+    align='left',
+    baseline='middle',
+    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+).encode(
+    text='lockdown_date:T'
 )
 
 
-combined = first_lockdown_start_line+new_and_cum_cases
-to_plot1 = combined.add_selection(picked).interactive().properties(width=800)
-st.write(to_plot1)
+text_second_lockdown = second_lockdown_start.mark_text(
+    align='left',
+    baseline='middle',
+    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+).encode(
+    text='lockdown2_date:T'
+)
+
+lockdown1_end = datetime.datetime(2020, 7, 27)
+df_cases_by_date['lockdown1_end'] = lockdown1_end 
+first_lockdown_end = alt.Chart(df_cases_by_date).mark_rule().encode(
+    x='lockdown1_end', color=alt.value('black'))
+
+
+text_first_lockdown_end = first_lockdown_end.mark_text(
+    align='left',
+    baseline='middle',
+    dx=3  # Nudges text to right so it doesn't appear on top of the bar
+).encode(
+    text='lockdown1_end:T'
+)
+
+
+lockdown2_end = datetime.datetime(2021, 1, 18)
+df_cases_by_date['lockdown2_end'] = lockdown2_end 
+second_lockdown_end = alt.Chart(df_cases_by_date).mark_rule().encode(
+    x='lockdown2_end', color=alt.value('black'))
+
+
+text_second_lockdown_end = second_lockdown_end.mark_text(
+    align='left',
+    baseline='middle',
+    dx=3,  # Nudges text to right so it doesn't appear on top of the bar
+    dy=20
+).encode(
+    text='lockdown2_end:T'
+)
+
+
+
+
+
+
+df_cases_by_date['lockdown2_date'] = lockdown2_date 
+second_lockdown_start = alt.Chart(df_cases_by_date).mark_rule().encode(
+    x='lockdown2_date', color=alt.value('green'),
+    tooltip=['lockdown2_date'])
+
+
+combined = new_and_cum_cases+first_lockdown_start+second_lockdown_start +text_first_lockdown + text_second_lockdown + first_lockdown_end + text_first_lockdown_end + second_lockdown_end + text_second_lockdown_end
+to_plot1 = combined.interactive().properties(width=800, title='COVID-19 in Nigeria as at 3rd of March 2020 -- Green and black lines represent start and end of major restrictions resp.')
+st.write(make_selector | to_plot1)
+
+
+#Situation in Nigeria at a Glance Ends
